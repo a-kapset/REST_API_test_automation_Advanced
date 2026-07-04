ba3a606a80897204e0383f893bb5ee2c343ce826
Artsiom K. <artemkapset@gmail.com>
2026-07-04 12:12:07 +0200

    feat: check_response_values() implemented and added to test_post_v1_account_base

--- tests/functional/post_v1_account/test_post_v1_account_base.py
+++ tests/functional/post_v1_account/test_post_v1_account_base.py
@@ -1,13 +1,4 @@
-from datetime import datetime
-from hamcrest import (
-    assert_that,
-    has_property,
-    has_properties,
-    starts_with,
-    all_of,
-    instance_of,
-    equal_to
-)
+from checkers.post_v1_account_checker import PostV1AccountChecker
 
 
 def test_post_v1_account_base(account_helper_fxt, user_data_fxt):
@@ -19,20 +10,4 @@ def test_post_v1_account_base(account_helper_fxt, user_data_fxt):
     account_helper_fxt.activate_user(login=login)
     response = account_helper_fxt.user_login(login=login, password=password, remember_me=True, validate_response=True)
 
-    assert_that(
-        response, all_of(
-            has_property('resource', has_property('login', starts_with('ab'))),
-            has_property('resource', has_property('registration', instance_of(datetime))),
-            has_property('resource', has_properties({
-                        'rating': has_properties(
-                            {
-                                'enabled': equal_to(True),
-                                'quality': equal_to(0),
-                                'quantity': equal_to(0)
-                            }
-                        )
-                    }
-                )
-            )
-        )
-    )
+    PostV1AccountChecker.check_response_values(response)
